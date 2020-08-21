5566fe7443877dc769eb63ef51230089c102c74f
Solimar Silva <solimarss@hotmail.com>
2020-08-21 10:08:22 -0300

    first commit

---
+++ src/test/java/solimar/silva/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package solimar.silva;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
